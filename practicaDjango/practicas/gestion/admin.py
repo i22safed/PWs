ce8ba75ca7728b6ac69a6903c508e057d2c2346b
David <i22safed@uco.es>
2017-07-06 21:02:49 +0200

    Añadidos los modelos de Equipo y Jugador

---
+++ practicaDjango/practicas/gestion/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from gestion.models import Equipo, Jugador
+
+# Register your models here.
+
+admin.site.register(Equipo)
+admin.site.register(Jugador)
---
+++ practicaDjango/practicas/gestion/models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Equipo(models.Model):
+    name = models.CharField(primary_key=True, max_length=25)
+    foundation = models.IntegerField()
+    city = models.CharField(max_length=25)
+    stadium = models.CharField(max_length=25)
+    coach = models.CharField(max_length=25)
+    capacity = models.IntegerField()
+
+    def storage(self):
+        self.save()
+
+    def __unicode__(self):
+        return selfname
+
+class Jugador(models.Model):
+    team = models.ForeignKey('Equipo')
+    playerName = models.CharField(max_length=25)
+    nacionality = models.CharField(max_length=3)
+    age = models.IntegerField()
+    heigh = models.IntegerField()
+    position = models.CharField(max_length=3)
+
+    def storage():
+        self.save()
+
+    def __unicode__(self):
+        return self.playerName
